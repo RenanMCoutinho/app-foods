c145a31b57ad35c091de56d153a4271a61bd4df4
Renan Marcilio Coutinho <renan.coutinho.11@gmail.com>
2025-06-27 23:16:30 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCVHE_HC0Vw8Ng_W2DSJh8t3CWSVYrmNK4",
+  authDomain: "app-foods-916ab.firebaseapp.com",
+  projectId: "app-foods-916ab",
+  storageBucket: "app-foods-916ab.firebasestorage.app",
+  messagingSenderId: "177875639667",
+  appId: "1:177875639667:web:4b12c3044b24c5f536b906",
+  measurementId: "G-S3M7867HPK"
+};
