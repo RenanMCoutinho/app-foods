0c830b6347119e20024ccffb8e342aff772767b5
renan.coutinho <renan.coutinho.11@gmail.com>
2026-03-06 15:07:54 -0300

    Ajuste.

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCVHE_HC0Vw8Ng_W2DSJh8t3CWSVYrmNK4",
-    authDomain: "app-foods-916ab.firebaseapp.com",
-    projectId: "app-foods-916ab",
-    storageBucket: "app-foods-916ab.appspot.com",
-    messagingSenderId: "177875639667",
-    appId: "1:177875639667:web:4b12c3044b24c5f536b906",
-    measurementId: "G-S3M7867HPK"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
